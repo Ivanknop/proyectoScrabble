9d20330f938d44c27756eb1ec8031882d5d53c09
Iván Knopoff <59924602+Ivanknop@users.noreply.github.com>
2020-05-27 22:06:29 -0300

    Timer con interfaz

---
+++ codigo/logica/timer.py
@@ -0,0 +1,52 @@
+import PySimpleGUI as sg
+import time
+
+# ----------------  Create Form  ----------------
+layout = [[sg.Text('', background_color='black')],
+    [sg.Text('00:00', size=(30, 1), font=('Helvetica', 30), justification='center', text_color='white', key='text', background_color='black')],
+    [sg.Text('', background_color='black')],
+    [sg.Button('Pause', key='button', button_color=('white', '#001480'), pad=((80,4),2)),
+     sg.Button('Reset', button_color=('white', '#007339'), key='Reset'),
+     sg.Exit(button_color=('white', '#8B1A1A'), key='Exit', )], ]
+
+
+window = sg.Window('Timer', background_color='black').Layout(layout)
+
+# ----------------  Event Loop  ----------------
+current_time = 0
+paused = False
+start_time = int(round(time.time() * 100))
+
+while (True):
+    # --------- Read and update window --------
+    if not paused:
+        event, values = window.Read(timeout=0)
+        current_time = int(round(time.time() * 100)) - start_time
+    else:
+        event, values = window.Read()
+    '''if event == 'button':
+        event = window.FindElement(event).GetText()
+    # --------- Do Button Operations --------
+    if event is None or event == 'Exit':        # ALWAYS give a way out of program
+        break
+    elif event is 'Reset':
+        start_time = int(round(time.time() * 100))
+        current_time = 0
+        paused_time = start_time
+    elif event == 'Pause':
+        paused = True
+        paused_time = int(round(time.time() * 100))
+        element = window.FindElement('button')
+        element.Update(text='Run')
+    elif event == 'Run':
+        paused = False
+        start_time = start_time + int(round(time.time() * 100)) - paused_time
+        element = window.FindElement('button')
+        element.Update(text='Pause')
+'''
+    # --------- Display timer in window --------
+    window.FindElement('text').Update('{:02d}:{:02d}.{:02d}'.format((current_time // 100) // 60,
+                                                                  (current_time // 100) % 60,
+                                                                  current_time % 100))
+# --------- After loop --------
+window.Close()
