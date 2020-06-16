ce971183ec6a1e05ee77794d0be0ba91f22cf24c
elrecursante <vilardebodiego@gmail.com>
2020-06-15 21:27:03 -0300

    se creo el modulo y clase jugador en codigo/logica, se retiro dicha clase dle modulo puntuacionez_mximas

---
+++ codigo/logica/jugador.py
@@ -0,0 +1,32 @@
+class Jugador():
+
+    def __init__(self, nombre, puntaje, dificultad ='facil', avatar = None):
+        self.__nombre = nombre
+        self.__puntuacion = puntaje
+        self.__dificultad = dificultad
+        self.__avatar = avatar
+
+
+    def __str__(self):
+        return 'Jugador: {} - Puntuación:  {}'.format(self.nombre, self.puntuacion)
+
+    def infoJugador(self):
+        info = ( self.__nombre ,
+                self.__puntuacion ,
+                self.__dificultad ,
+                self.__avatar ,
+                    )
+
+        return info
+    
+    def getNombre(self):
+        return self.__nombre
+
+    def getPuntos(self):
+        return self.__puntuacion
+
+    def getdificultad(self):
+        return self.__dificultad
+
+    def getAvatar(self):
+        return self.__avatar
