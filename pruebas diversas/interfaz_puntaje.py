0186c6bd6a38a53b3385772c0c141b7b6949d89d
Iván Knopoff <59924602+Ivanknop@users.noreply.github.com>
2020-06-27 11:17:04 -0300

    Update interfaz_puntaje.py

--- pruebas diversas/interfaz_puntaje.py
+++ pruebas diversas/interfaz_puntaje.py
@@ -1,10 +1,6 @@
 import PySimpleGUI as sg
 from puntuaciones_maximas import*
 
-
-def textos (pos):
-    return listados[pos].Text
-
 def actualizar_puntaje (puntaje):
     listado_puntos =''
     for p in range(len(puntaje.puntajes)): 
@@ -15,16 +11,16 @@ def blanquear (puntaje):
     puntaje._vaciar_puntajes()
     puntaje.guardar()
     actualizar_puntaje(puntaje)
-
 contenido = [
     [sg.Text('Puntuaciones máximas',size=(20,1),font=('Impact',18),text_color=('black'),key='_puntos')],
     [sg.Text(key='puntos',size=(20,10),justification='center',background_color='Black',text_color='white')],
     [sg.Button('Borrar puntuación',font=('Arial',8),size=(20,3),key='blanquear'),    
-    sg.Button('Reestablecer puntuación',font=('Arial',8),size=(20,3),key='reestablecer')]
-]
+    sg.Button('Reestablecer puntuación',font=('Arial',8),size=(20,3),key='reestablecer')]   
+    ]
+ventana = sg.Window ('Puntaje Máximo',layout=contenido,size= (400,400))
+ventana.finalize()
 def mostrar_ventana ():
-    ventana = sg.Window ('Puntaje Máximo',layout=contenido,size= (400,400))
-    ventana.finalize()
+
     while True:    
         puntuaciones = Puntuacion_Maxima()
         actualizar_puntaje(puntuaciones)
@@ -38,3 +34,4 @@ def mostrar_ventana ():
             puntuaciones.inicializar_puntuacion()
             actualizar_puntaje(puntuaciones)
     ventana.close()
+mostrar_ventana()
