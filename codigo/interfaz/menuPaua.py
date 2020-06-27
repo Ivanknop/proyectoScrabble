e638333fdedf84041f306629bd67aae9370349da
elrecursante <vilardebodiego@gmail.com>
2020-06-26 21:43:46 -0300

    se agrego la interfazde PAUSA , sin IMPLEMENTAR

---
+++ codigo/interfaz/menuPaua.py
@@ -0,0 +1,33 @@
+import PySimpleGUI as sg
+import os
+
+#evaluar si es un objeto menu
+
+def menu_pausa():
+
+    img_boton_largo = os.path.join('media', 'media_ii', 'botonlargo.png')
+    img_logo = os.path.join('media', 'scrabbleArLogo.png')
+    layout = [[sg.Image(img_logo, background_color='#4f280a')],
+              [sg.Button('Retomar', image_filename=img_boton_largo, button_color=('black', '#4f280a'), border_width=0, font=('Italic 24'),
+                         size=(20, 3), key='retomar')],
+              [sg.Button('Guardar', image_filename=img_boton_largo, border_width=0,button_color=('black', '#4f280a'), font=('Italic 24'),
+                         size=(20, 3), key='guardar')],
+              [sg.Button('Abandonar', image_filename=img_boton_largo, border_width=0,button_color=('black', '#4f280a'), font=('Italic 24'), size=(20, 3),
+                         key='abandonar')],
+
+              ]
+
+    ventana = sg.Window('pausa',layout=layout, background_color='#4f280a', no_titlebar=False,element_justification='center')
+
+
+    while True:
+        event, value= ventana.read()
+
+        if event ==  None:
+            break
+
+
+    ventana.close()
+
+if __name__ == '__main__':
+    menu_pausa()
