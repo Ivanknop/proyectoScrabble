426523d354cb13a1c4b93eaf50baea0e09834cdb
elrecursante <vilardebodiego@gmail.com>
2020-06-29 23:28:11 -0300

    RETOQUES DE INTERFAZ

--- codigo/interfaz/dibujar.py
+++ codigo/interfaz/dibujar.py
@@ -96,7 +96,7 @@ class Dibujar():
                             sg.Button(image_filename=f'{self._directorio_media}bolsallenaP.png', border_width=0, key='cambiar')]]
         #Crea la ventana y la muestra
         diseño = [top,[sg.Column(columna_izquierda,background_color='#ece6eb',justification='left'), sg.Column(columna_derecha, element_justification='center',justification='right', pad=(10, None))]]
-        self._interfaz = sg.Window('ScrabbleAR', diseño, resizable=False)
+        self._interfaz = sg.Window('ScrabbleAR', diseño, resizable=False, no_titlebar=False)
         self._interfaz.Finalize()
 
     def tema_tablero(self):
