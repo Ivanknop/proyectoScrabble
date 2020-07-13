6c35a870d535077d1d6456e8b0a13056e615f107
elrecursante <vilardebodiego@gmail.com>
2020-07-13 18:11:42 -0300

    correcciones esteticas en la interfaz de puntaje

--- codigo/interfaz/interfaz_puntaje.py
+++ codigo/interfaz/interfaz_puntaje.py
@@ -17,14 +17,14 @@ def puntajes():
 
     contenido = [
         [sg.Text('Puntuaciones máximas',size=(20,1),font=('Impact',18),text_color=('black'),key='_puntos')],
-        [sg.Text(key='puntos',size=(200,10),justification='center',font=('Arial',16),background_color='Black',text_color='white')],
+        [sg.Text(key='puntos',size=(200,10),justification='center',font=('Arial',18),background_color='Black',text_color='white')],
         [sg.Button('Borrar puntuación',font=('Arial',16),size=(10,3),button_color=('black', '#f75404'),key='blanquear'),
         sg.Button('Reestablecer puntuación',font=('Arial',16),size=(10,3),button_color=('black', '#f75404'),key='reestablecer'),
-         sg.Button('volver', font=('Arial', 16), size=(10, 3),button_color=('black', '#f75404'), key='volver') ]
+         sg.Button('volver', font=('Arial', 16), size=(14, 3),button_color=('black', '#f75404'), key='volver') ]
 
         ]
     mi_tema()
-    ventana = sg.Window ('Puntaje Máximo',layout=contenido,size= (400,400), no_titlebar=False)
+    ventana = sg.Window ('Puntaje Máximo',layout=contenido,size= (420,400), no_titlebar=True, keep_on_top=True)
     ventana.finalize()
 
     while True:
