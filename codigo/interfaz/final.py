116ed14e0554c90fac9ec44daf7f2bfa0410ef62
Iván Knopoff <59924602+Ivanknop@users.noreply.github.com>
2020-07-14 22:53:24 -0300

    mejorada interfaz final. Enlazado pje y lista palabras

--- codigo/interfaz/final.py
+++ codigo/interfaz/final.py
@@ -23,25 +23,30 @@ def terminar(nombre, punt_jug,punt_pc,pal_jug,pal_pc):
         [sg.Text('PUNTAJE DE LA PC',size=(40,1),font=('Impact',14),justification='center',text_color=('#D09F61'),key='_pc')],
         [sg.Text(key='pje_pc',size=(50,1),justification='center',font=('Arial',50),background_color='Black',text_color='white')],
         [sg.Text(key='ganador',size=(50,1),justification='center',font=('Arial',20),background_color='Black',text_color='white')],
-        [sg.Button('Salir', font=('Arial', 16), size=(10, 1),button_color=('black', '#f75404'), key='salir') ]
+        [sg.Text('',size=(5,1)),
+        sg.Button('Salir', font=('Arial', 10), size=(10, 2),button_color=('black', '#f75404'), key='salir'),
+        sg.Button('Listado de palabras', font=('Arial', 10), size=(10, 2),button_color=('black', '#f75404'), key='list_pal'),
+        sg.Button('Puntuaciones Máximas', font=('Arial', 10), size=(10, 2),button_color=('black', '#f75404'), key='puntajes')]
 
         ]
     mi_tema()
     ven = sg.Window ('Ganador',layout=contenido,size= (400,400), no_titlebar=False)
     ven.finalize()
 
-    while True:
-        #Crea un puntaje, actualiza las puntuaciones máximas y abre la ventana de puntuaciones máximas
-        puntaje = Puntuacion_Maxima()
-        puntaje.agregar(Jugador(nombre,punt_jug))
-        puntajes()        
-        #Abre la ventana de las palabras utilizadas
-        listado_palabras(pal_jug,pal_pc)
+    while True:         
         #Abre la ventana donde se ve quién ganó
         ver_ganador (punt_jug,punt_pc,ven)
         event, values = ven.read()
         if event in (None,'salir'):
             break
+        if event == 'list_pal':
+            #Abre la ventana de las palabras utilizadas
+            listado_palabras(pal_jug,pal_pc)
+        if event == 'puntajes':
+            #Crea un puntaje, actualiza las puntuaciones máximas y abre la ventana de puntuaciones máximas
+            puntaje = Puntuacion_Maxima()
+            puntaje.agregar(Jugador(nombre,punt_jug))
+            puntajes()  
     ven.close()
 if __name__ == '__main__':
     terminar()
